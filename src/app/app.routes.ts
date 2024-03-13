aa08ed8fd11d476a7067db5e6b0ffb228fa487a4
Thales Silva <bliteti.thalessilva@unidas.com.br>
2024-03-13 17:52:27 -0300

    finished header desktop layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './shared/components/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    title: 'About',
+    component: AboutComponent
+  },
+];
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,21 +1,24 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule,],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   headerLinks = [
-    {link: 'ABOUT'},
-    {link: 'ARTICLES'},
-    {link: 'PROJECTS'},
-    {link: 'TALKS'},
-    {link: 'PODCASTS'},
-    {link: 'INVESTING'},
-    {link: 'USES'},
-    {link: 'REMINDER'},
+    {link: 'ABOUT', href: '/about'},
+    {link: 'ARTICLES', href: '/articles'},
+    {link: 'PROJECTS', href: '/projects'},
+    {link: 'TALKS', href: '/taalks'},
+    {link: 'PODCASTS', href: '/podcasts'},
+    {link: 'INVESTING', href: '/investing'},
+    {link: 'USES', href: '/uses'},
+    {link: 'REMINDER', href: '/reminder'},
   ]
 }
