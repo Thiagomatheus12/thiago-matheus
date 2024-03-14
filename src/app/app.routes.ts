31d5b60b99c0f3c3b7f109228eb240665611924d
Thales Silva <bliteti.thalessilva@unidas.com.br>
2024-03-14 17:52:33 -0300

    :sparkles: feature - header mobile and starting body

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { AboutComponent } from './shared/components/about/about.component';
+import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    component: HomeComponent
+  },
   {
     path: 'about',
     title: 'About',
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule,],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
   headerLinks = [
     {link: 'ABOUT', href: '/about'},
     {link: 'ARTICLES', href: '/articles'},
