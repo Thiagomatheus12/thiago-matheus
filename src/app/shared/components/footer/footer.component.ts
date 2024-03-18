b74d2a85b7ff650f727db4e15992b38720d8138f
Thales Silva <bliteti.thalessilva@unidas.com.br>
2024-03-18 17:20:21 -0300

    :sparkles: feature - finish footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,20 +1,20 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
   footerLinks = [
-    {link: 'email', href: '/email'},
-    {link: 'twitter', href: '/twitter'},
-    {link: 'github', href: '/github'},
-    {link: 'linkedin', href: '/linkedin'},
-    {link: 'instagram', href: '/instagram'},
+    {link: 'email', href: 'mailto:thiagomtsdev.contato@gmail.com', src: '/assets/icons/email.png'},
+    {link: 'github', href: 'https://github.com/Thiagomatheus12', src: '/assets/icons/github.png'},
+    {link: 'linkedin', href: 'https://www.linkedin.com/in/thiago-matheus-99b98b239/', src: '/assets/icons/linkedin.png'},
+    {link: 'instagram', href: 'https://www.instagram.com/thiagomts01/', src: '/assets/icons/instagram.png'},
   ]
 
 }
