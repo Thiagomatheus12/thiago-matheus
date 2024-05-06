db075846c9c8f1a72b28f4147a15c649fdbdce6b
Thales Silva <bliteti.thalessilva@unidas.com.br>
2024-05-06 17:00:44 -0300

    :sparkles: feature -  folder organization and dialog style

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { HEADER_LINKS } from '../../constantes/headerLinks';
 
 
 @Component({
@@ -16,14 +17,5 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent {
 
-  headerLinks = [
-    {link: 'ABOUT', href: '/about'},
-    {link: 'ARTICLES', href: '/articles'},
-    {link: 'PROJECTS', href: '/projects'},
-    {link: 'TALKS', href: '/taalks'},
-    {link: 'PODCASTS', href: '/podcasts'},
-    {link: 'INVESTING', href: '/investing'},
-    {link: 'USES', href: '/uses'},
-    {link: 'REMINDER', href: '/reminder'},
-  ]
+  headerLinks = HEADER_LINKS
 }
